0aca6ad63c225cef6a911ae9847804fe03430458
bubonka4444 <buneev.18.03@gmail.com>
2023-05-20 00:31:26 +0400

    Добавьте файлы проекта.

---
+++ TP_LW5/Controllers/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_LW5.Models;
+
+namespace TP_LW5.Controllers
+{
+    public class PersonController : Controller
+    {
+        private List<Person> _people;
+
+        public PersonController()
+        {
+            _people = new List<Person>();
+        }
+
+        public IActionResult Index()
+        {
+            return View(_people);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Person person)
+        {
+            _people.Add(person);
+            HttpContext.Response.Cookies.Append("CurrentPersonIndex", _people.Count.ToString());
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var person = _people[id];
+            HttpContext.Response.Cookies.Append("CurrentPersonIndex", id.ToString());
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Person person)
+        {
+            var id = int.Parse(HttpContext.Request.Cookies["CurrentPersonIndex"]);
+            _people[id] = person;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TP_LW5/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TP_LW5.Models
+{
+    public class Person
+    {
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Фамилия")]
+        public string LastName { get; set; }
+
+        [DisplayName("Возраст")]
+        public int Age { get; set; }
+
+        [DisplayName("Стаж")]
+        public int Experience { get; set; }
+
+        [DisplayName("Специальность")]
+        public string Specialization { get; set; }
+
+        [DisplayName("Номер телефона")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
